823db997ebc2873d965d417fc44ccc9f2eb8a0f6
Agentformula <justin@agentformula.com>
2016-02-18 14:15:46 -0800

    stashing changes

--- lib/rubillow/models/reviews.rb
+++ lib/rubillow/models/reviews.rb
@@ -4,22 +4,28 @@ module Rubillow
     class Reviews < Base
       
       # @return [String] region id.
-      attr_accessor :region_id
+      attr_accessor :reviews
       
       # @return [Array] postings with MakeMeMove status ({Models::Posting}).
-      attr_accessor :make_me_move
+      attr_accessor :photo
       
       # @return [Array] postings with FSBA status ({Models::Posting}).
-      attr_accessor :for_sale_by_owner
+      attr_accessor :reviews_url
       
       # @return [Array] postings with FSBO status ({Models::Posting}).
-      attr_accessor :for_sale_by_agent
+      attr_accessor :review_link_text
       
       # @return [Array] postings with reporting status ({Models::Posting}).
-      attr_accessor :report_for_sale
+      attr_accessor :position
       
       # @return [Array] postings with for rent status ({Models::Posting}).
-      attr_accessor :for_rent
+      attr_accessor :company
+      
+      attr_accessor :specialties
+      
+      attr_accessor :service_areas
+      
+      attr_accessor :local_knowledge_rating, :process_expertise_rating, :responsiveness_rating, :negotiation_skill_rating
       
       protected
       
@@ -28,6 +34,8 @@ module Rubillow
         super
         
         return if !success?
+        
+        @reviews = @parser.xpath('//review')
         binding.pry
       end
     end
