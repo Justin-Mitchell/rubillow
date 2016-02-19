fc3c8baacf113caa16f44bd541a529a727d6e043
Justin-Mitchell <jmitchell4140@gmail.com>
2016-02-18 16:35:56 -0800

    setup parser, testing

--- lib/rubillow/models/reviews.rb
+++ lib/rubillow/models/reviews.rb
@@ -34,9 +34,29 @@ module Rubillow
         super
         
         return if !success?
-        
-        @reviews = @parser.xpath('//review')
-        binding.pry
+        reviews = []
+        @parser.xpath('//review').each_with_index do |data, i|
+          review = {
+            index: i,
+            created_at: data.xpath("//reviewDate")[i].text,
+            reviewer: data.xpath("//reviewer")[i].text,
+            reviewer_link: data.xpath("//reviewerLink")[i].text,
+            summary: data.xpath("//reviewSummary")[i].text,
+            review: data.xpath("//description")[i].text,
+            url: data.xpath("//reviewURL")[i].text,
+            response: data.xpath("//reviewResponse")[i].text,
+            ratings: {
+              general: data.xpath("//rating")[i].text,
+              local_knowledge: data.xpath("//localknowledgeRating")[i].text,
+              process_expertise: data.xpath("//processexpertiseRating")[i].text,
+              responsiveness: data.xpath("//responsivenessRating")[i].text,
+              negotiation_skills: data.xpath("//negotiationskillsRating")[i].text,
+            }
+          }
+          
+          data << review
+        end
+        @reviews = data
       end
     end
   end
